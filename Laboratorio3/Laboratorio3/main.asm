fd8175fcde1f80721720b0c0726e3bfc000f9918
alma-mata <mat23038@uvg.edu.gt>
2025-02-20 22:01:24 -0600

    Inicio de laboratorio
    Se crean los archivos del laboratorio 3

---
+++ Laboratorio3/Laboratorio3/main.asm
@@ -0,0 +1,12 @@
+;-----------------------------------------------
+; Universidad del Valle de Guatemala
+; IE2023: Programacion de Microcontroladores
+; Laboratorio3.asm
+
+; Autor: Alma Mata Ixcayau
+; Proyecto: Laboratorio 3. Interrupciones
+; Descripcion: Contador binario de 4 bits que cambia con interrupciones.
+; Hardware: ATMEGA328P
+; Creado: 20/02/2025
+; Ultima modificacion: --
+;-----------------------------------------------
